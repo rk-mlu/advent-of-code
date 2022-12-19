11bf0aaf676763a79322167cd65a31bbac16a615
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2022-12-19 20:11:05 +0100

    accepted solution to day 11

--- day11/monkey.py
+++ day11/monkey.py
@@ -1,17 +1,20 @@
 """ Advent of Code 2022 -- Day 11 -- """
 
 import aoc
+import numpy as np
 
 class Monkey:
     def __init__(self, num):
         self.num = num
         self.items = []
+        self.items_rk = []
         self.icount = 0
         self.operator = None
         self.operand = None
         self.o_divisor = 3
         self.t_divisor = None
         self.targets = None
+        self.primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
 
     def __str__(self):
         s = f'Monkey No {self.num}'
@@ -26,18 +29,34 @@ class Monkey:
             return (wl**2)//self.o_divisor
     
     def test(self, wl):
+        # j = self.primes.index(self.t_divisor)
         if wl % self.t_divisor == 0:
             return self.targets[0]
         else :
             return self.targets[1]
 
+    def op_rk(self, wl):
+        if self.operator == '+':
+            wl_rk = wl + self.operand
+        if self.operator == '*':
+            wl_rk = wl * self.operand
+        if self.operator == '**':
+            wl_rk = wl**2
 
-if __name__ == '__main__':
-    # data = aoc.get_input('input.txt')                                  
-    data = aoc.get_input('input2.txt')
-    
-    # Part I    
-    num_rounds = 20
+        for j ,p in enumerate(self.primes):
+            wl_rk[j] = wl_rk[j] % p
+
+        return wl_rk
+
+    def test_rk(self, wl):
+        j = self.primes.index(self.t_divisor)
+        if wl[j] == 0:
+            return self.targets[0]
+        else :
+            return self.targets[1]
+
+
+def parsing(data, odiv=3):
     monkeys = {}
     
     # parsing
@@ -49,10 +68,18 @@ if __name__ == '__main__':
                      
         if line.startswith('  Starting items:'):
             for w in words[2:]:
-                M.items.append(int(w.strip(",")))
+                
+                wl = int(w.strip(","))
+                M.items.append(wl)
+
+                wl_rk = np.zeros(len(M.primes), dtype=int)
+                for j ,p in enumerate(M.primes):
+                    wl_rk[j] = wl % p
+                M.items_rk.append(wl_rk)
 
         if line.startswith('  Operation:'):
             M.operator = words[-2]
+            M.o_divisor = odiv
             if words[-1].isnumeric():
                 M.operand = int(words[-1])
             else :
@@ -69,6 +96,18 @@ if __name__ == '__main__':
             t2 = int(words[-1])
             M.targets = (t1, t2)
             monkeys[M.num] = M
+    
+    return monkeys
+
+
+
+if __name__ == '__main__':
+    data = aoc.get_input('input.txt')                                  
+    # data = aoc.get_input('input2.txt')
+    
+    # Part I    
+    monkeys = parsing(data)
+    num_rounds = 20
  
     for r in range(num_rounds):
         print(f'Round {r}')
@@ -84,52 +123,21 @@ if __name__ == '__main__':
             print(M.icount)
 
     # Part II    
-    num_rounds = 20
-    monkeys2 = {}
+    monkeys2 = parsing(data, 1)
+    num_rounds = 10000
     
-    # parsing
-    for line in data.splitlines():
-        words = line.split()
-        if line.startswith('Monkey'):
-            num = int(words[-1][:-1])
-            M = Monkey(num)
-                     
-        if line.startswith('  Starting items:'):
-            for w in words[2:]:
-                M.items.append(int(w.strip(",")))
-
-        if line.startswith('  Operation:'):
-            M.operator = words[-2]
-            M.o_divisor = 1
-            if words[-1].isnumeric():
-                M.operand = int(words[-1])
-            else :
-                M.operand = 2
-                M.operator = '**'
-        
-        if line.startswith('  Test'):
-            M.t_divisor = int(words[-1])
-
-        if line.startswith('    If true:'):
-            t1 = int(words[-1])
-
-        if line.startswith('    If false:'):
-            t2 = int(words[-1])
-            M.targets = (t1, t2)
-            monkeys2[M.num] = M
  
     for r in range(num_rounds):
-        print(f'Round {r+1}')
         for m in range(len(monkeys2.keys())):
             M = monkeys2[m]
-            for wl in M.items:
+            for wl in M.items_rk:
                 M.icount += 1
-                new = M.op(wl)
-                tar = M.test(new)
-                monkeys2[tar].items.append(new)
-            M.items = []
+                new = M.op_rk(wl)
+                tar = M.test_rk(new)
+                monkeys2[tar].items_rk.append(new)
+            M.items_rk = []
        
-        if r in {0, 19, 999, 1999, 2999, 3999}:
+        if r in {0, 19, 999, 1999, 2999, 3999, 9999}:
             for m in range(len(monkeys2.keys())):
                 M = monkeys2[m] 
                 print(M, M.icount)
