53b6f7ec9ba6b86e5307508a3aa8518433c68bb5
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2022-12-20 22:13:21 +0100

    accepted solution for day 20

--- day20/gps.py
+++ day20/gps.py
@@ -5,10 +5,57 @@ import aoc
 
 def parsing(data):
     lines = data.splitlines()
+    
+    numbers = [ (j,int(n)) for j, n in enumerate(lines)]
 
-    print(lines)
+    return numbers
 
-    return lines
+
+def decrypt(numbers):
+    N = len(numbers)
+    new_numbers = numbers.copy()
+    
+    # print_numbers(new_numbers)
+
+    for j, n in numbers:
+        if n == 0:
+            zero = (j,n)
+            continue
+
+        k = new_numbers.index((j,n))
+        new_numbers.pop(k)
+
+        i = (k + n ) % (N-1)
+               
+        print(f"{n} moves between {new_numbers[i-1][1]} and {new_numbers[i][1]}:")
+        new_numbers.insert(i, (j,n))
+
+        # print_numbers(new_numbers)
+            
+    return new_numbers, zero
+
+def print_numbers(numbers):
+    s = []
+    for j, n in numbers:
+        s.append(n)
+    print(s)
+
+def sum_coord(numbers, zero):
+
+    k = numbers.index(zero)
+    print(k)
+    
+    k1000 = (k + 1000) % N
+    k2000 = (k + 2000) % N
+    k3000 = (k + 3000) % N
+    
+    summe = numbers[k1000][1] + numbers[k2000][1] + numbers[k3000][1]
+    s = 'Part I: '
+    s += f'{numbers[k1000][1]} + {numbers[k2000][1]} + {numbers[k3000][1]}'
+    s += f' = {summe}' 
+
+    print(s)
+
 
 
 if __name__ == '__main__':
@@ -17,6 +64,10 @@ if __name__ == '__main__':
     
     # Part I
     numbers = parsing(data)
+    N = len(numbers)
 
+    num_new, zero = decrypt(numbers)
+
+    sum_coord(num_new, zero)
 
     # Part II
