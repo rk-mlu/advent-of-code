2dc907a45d8ccd0f133a31ee527133cf0443b754
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2022-12-20 19:26:17 +0100

    renamed a file

--- day20/gps.py
+++ day20/gps.py
@@ -1,12 +1,22 @@
-""" Advent of Code 2022 -- Day 11 -- """
+""" Advent of Code 2022 -- Day 20 -- """
 
 import aoc
 # import numpy as np
+
+def parsing(data):
+    lines = data.splitlines()
+
+    print(lines)
+
+    return lines
+
 
 if __name__ == '__main__':
     data = aoc.get_input('input.txt')                                  
     # data = aoc.get_input('input2.txt')
     
-    # Part I    
-    
+    # Part I
+    numbers = parsing(data)
+
+
     # Part II
