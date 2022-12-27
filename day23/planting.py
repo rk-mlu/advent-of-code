75a01a66aa281e1674528b45e57ebe0df00b058d
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2022-12-27 14:15:49 +0100

    prepared files for day 23

---
+++ day23/planting.py
@@ -0,0 +1,12 @@
+""" Advent of Code 2022 -- Day 23 -- """
+
+import aoc
+# import numpy as np
+
+if __name__ == '__main__':
+    data = aoc.get_input('input.txt')                                  
+    # data = aoc.get_input('input2.txt')
+    
+    # Part I    
+    
+    # Part II
