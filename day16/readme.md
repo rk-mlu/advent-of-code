dfe80fbf718a76101bf1a33c5550fff6952674f1
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-01-06 20:35:53 +0100

    Accepted solution for day 16, part II

---
+++ day16/readme.md
@@ -0,0 +1,60 @@
+# Day 16
+
+The computing time of my solution is rather long. Below I note the output
+for both parts for the input in `input.txt`.
+
+## Part I
+
+Pressure released 1376 along the path:
+- Minute  0: position AA
+- Minute  2: position HS
+- Minute  3: position HS (opened)
+- Minute  6: position EG
+- Minute  7: position EG (opened)
+- Minute  9: position TJ
+- Minute 10: position TJ (opened)
+- Minute 12: position EG
+- Minute 15: position HO
+- Minute 16: position HO (opened)
+- Minute 19: position EG
+- Minute 22: position HS
+- Minute 25: position IQ
+- Minute 26: position IQ (opened)
+
+Part I: The most pressure possible to release is 1376
+
+**Computing time**: Less than a minute
+
+## Part II
+
+Pressure released 1933 along the path:
+- Minute  0: positions ('AA', 'AA')
+- Minute  1: positions ('VA', 'FB')
+- Minute  2: positions ('HS', 'FT')
+- Minute  3: positions ('HS', 'FT') (opened HS) (opened FT)
+- Minute  4: positions ('NY', 'DW')
+- Minute  5: positions ('UU', 'JH')
+- Minute  6: positions ('EG', 'JH') (opened JH)
+- Minute  7: positions ('EG', 'NK') (opened EG)
+- Minute  8: positions ('CK', 'DV')
+- Minute  9: positions ('TJ', 'DV') (opened DV)
+- Minute 10: positions ('TJ', 'NK') (opened TJ)
+- Minute 11: positions ('CK', 'JH')
+- Minute 12: positions ('EG', 'LS')
+- Minute 13: positions ('DS', 'YP')
+- Minute 14: positions ('LT', 'KS')
+- Minute 15: positions ('HO', 'KS') (opened KS)
+- Minute 16: positions ('HO', 'YP') (opened HO)
+- Minute 17: positions ('LT', 'LS')
+- Minute 18: positions ('DS', 'JH')
+- Minute 19: positions ('EG', 'QU')
+- Minute 20: positions ('UU', 'JV')
+- Minute 21: positions ('NY', 'WI')
+- Minute 22: positions ('HS', 'WI') (opened WI)
+- Minute 23: positions ('HB', 'GS')
+- Minute 24: positions ('AN', 'EV')
+- Minute 25: positions ('WI', 'EV') (opened EV)
+
+Part II: The most pressure possible to release is 1933
+
+**Computing time**: about 55 minutes
--- day16/valves.py
+++ day16/valves.py
@@ -3,6 +3,7 @@
 import aoc
 import copy
 import numpy as np
+from itertools import product, chain, pairwise, islice, combinations_with_replacement
 
 class Valve:
     def __init__(self, name, rate):
@@ -94,8 +95,9 @@ class Path:
         rates = np.array(rates)
         rates.sort()
         tl = np.arange(self.t)
+        tl = islice(tl[::-1], 0, None, 3)
         try: 
-            for (rate, t) in zip(rates[::-1], tl[::-1]):
+            for (rate, t) in zip(rates[::-1], tl):
                 r += t*rate
         except: 
             print(rates, tl)
@@ -148,12 +150,117 @@ def gen_paths(valves, p, closed, threshold=0):
 
     return best_path, max_pressure
 
+class Path2:
+    def __init__(self, start1, start2, tlim):
+        self.pos1 = start1
+        self.pos2 = start2
+        self.hist = [(start1, start2, tlim)]
+        self.t = tlim
+        self.pressure = 0
+        self.opened = []
+    
+    def __str__(self):
+        s = f'Pressure released {self.pressure} along the path:'
+        for j, (v1, v2, m) in enumerate(self.hist):
+            s += f'\nMinute {26-m:2}: positions {v1, v2}'
+            if v1 == self.hist[j-1][0]:
+                s+= f' (opened {v1})'
+            if v2 == self.hist[j-1][1]:
+                s+= f' (opened {v2})'
+        return s
+
+    def copy(self):
+        new_p = Path2(self.pos1, self.pos2, self.t)
+        new_p.hist = copy.deepcopy(self.hist)
+        new_p.pressure = self.pressure*1
+        new_p.opened = copy.deepcopy(self.opened)
+        return new_p
+
+    def remaining(self, valves):
+        # crude upper bound for remaing releasable pressure
+        r = 0
+        rates = []
+        for v in valves.keys():
+            if v not in self.opened:
+                rates.append(valves[v].rate)
+        rates = np.array(rates)
+        rates.sort()
+        rl = islice(pairwise(rates[::-1]), 0, None, 2)
+        tl = np.arange(self.t)
+        tl = islice(tl[::-1], 0, None, 3)
+        for (rate1, rate2), t in zip(rl, tl):
+            r += t*(rate1+rate2)
+        return r
+
+def gen_paths2(valves, p, closed, threshold=0):
+    max_pressure = max(threshold, p.pressure)
+    best_path = p
+    
+    if p.t == 0 or len(p.opened) == closed:
+        return best_path, max_pressure
+    
+    if p.pos1 == p.pos2:
+        l1 = chain.from_iterable([[valves[p.pos1].name], valves[p.pos1].connected.keys()])
+        it = combinations_with_replacement(l1, 2)
+    else :
+        l1 = chain.from_iterable([[valves[p.pos1].name], valves[p.pos1].connected.keys()])
+        l2 = chain.from_iterable([[valves[p.pos2].name], valves[p.pos2].connected.keys()])
+        it = product(l1, l2)
+
+    for nb1, nb2 in it:
+        pn = p.copy()
+        pn.t = p.t - 1
+
+        if nb1 != p.pos1:
+            if len(p.hist) > 1 and p.hist[-2][0] == nb1:
+                # do not move hence and forth
+                continue
+            if len(valves[nb1].connected) == 1 and nb1 in p.opened:
+                # do not enter already opened branches
+                continue
+            pn.pos1 = nb1
+        else :
+            if valves[p.pos1].rate > 0 and p.pos1 not in p.opened:
+                # check if valve can be opened
+                pn.opened.append(p.pos1)
+                pn.pressure += pn.t*valves[p.pos1].rate
+            else :
+                continue
+
+        if nb2 != p.pos2:
+            if len(p.hist) > 1 and p.hist[-2][1] == nb2:
+                # do not move hence and forth
+                continue
+            if len(valves[nb2].connected) == 1 and nb2 in pn.opened:
+                # do not enter already opened branches
+                continue
+            pn.pos2 = nb2
+        else :
+            if valves[p.pos2].rate > 0 and p.pos2 not in pn.opened:
+                # check if valve can be opened
+                pn.opened.append(p.pos2)
+                pn.pressure += pn.t*valves[p.pos2].rate
+            else :
+                continue
+            
+        pn.hist.append((pn.pos1, pn.pos2, pn.t))
+
+        if pn.pressure + pn.remaining(valves) > max_pressure:
+            # only follow path if remaining pressure is sufficent 
+            p_new, press_new = gen_paths2(valves, pn, closed, max_pressure)
+            if press_new > max_pressure:
+                max_pressure = press_new
+                best_path = p_new
+                print(f'New optimum found with pressure {max_pressure}')
+            
+    return best_path, max_pressure
 
 if __name__ == '__main__':
     data = aoc.get_input('input.txt')                                  
     # data = aoc.get_input('input2.txt')
     
     # Part I   
+    print('Part I: Computing optimal path...')
     valves, closed = parsing(data)
     num_closed = len(closed)
     reduced_valves = reduce_valves(valves)
@@ -162,7 +269,13 @@ if __name__ == '__main__':
     p, mp = gen_paths(reduced_valves, p, num_closed)
 
     print(p)
-    print(f'Part I: The most pressure possible to release is {p.pressure}')
+    print(f'Part I: The most pressure possible to release is {p.pressure}\n')
     
     # Part II
+    threshold = 1500
+    print('Part II: Computing optimal path...')
+    p2 = Path2('AA', 'AA', 26)
+    p2, mp2 = gen_paths2(valves, p2, num_closed, 1500)
 
+    print(p2)
+    print(f'Part II: The most pressure possible to release is {p2.pressure}')
