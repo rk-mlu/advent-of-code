09165169c6c0c3fdaff129f11a310ca7eabc8271
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-01-10 09:38:36 +0100

    day 19: parsing of input

--- day19/robots.py
+++ day19/robots.py
@@ -1,12 +1,51 @@
 """ Advent of Code 2022 -- Day 19 -- """
 
 import aoc
-# import numpy as np
+import numpy as np
+
+class Blueprint:
+    def __init__(self, costs):
+        self.ore_rob = costs[0]
+        self.clay_rob = costs[1]
+        self.obs_rob = costs[2]
+        self.geo_rob = costs[3]
+
+    def __str__(self):
+        s = f'Costs: Ore R {self.ore_rob}. Clay R {self.clay_rob}.'
+        s += f' Obsidian R {self.obs_rob}. Geode R {self.geo_rob}.'
+        return s
+
+
+def parsing(data):
+    blueprints = []
+
+    lines = data.splitlines()
+
+    for line in lines:
+        words = line.split()
+        costs = []
+        cost = np.array([int(words[6]), 0, 0, 0], dtype=int)
+        costs.append(cost)
+        cost = np.array([int(words[12]), 0, 0, 0], dtype=int)
+        costs.append(cost)
+        cost = np.array([int(words[18]), int(words[21]), 0, 0], dtype=int)
+        costs.append(cost)
+        cost = np.array([int(words[27]), 0, int(words[30]), 0], dtype=int)
+        costs.append(cost)
+        bluep = Blueprint(costs)
+        blueprints.append(bluep)
+
+    return blueprints
+
 
 if __name__ == '__main__':
     data = aoc.get_input('input.txt')                                  
     # data = aoc.get_input('input2.txt')
     
     # Part I    
+    blueprints = parsing(data)
+
+    for b in blueprints:
+        print(b)
     
     # Part II
