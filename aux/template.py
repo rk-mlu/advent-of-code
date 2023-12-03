42c6d9c17248c149dcdad3d43afbaa3e2b72629d
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-12-03 12:59:48 +0100

    minor update of the template

--- aux/template.py
+++ aux/template.py
@@ -9,12 +9,15 @@ if __name__ == '__main__':
     
     # Part I    
     data = aoc.get_input('input1.txt')                                  
+    lines = data.linesplit()
 
     ans1 = 0
-    print(f'Answer to part 1" {ans1}')
+
+    print(f'Answer to part 1: {ans1}')
 
     # Part II
     # data = aoc.get_input('input2.txt')
     
     ans2 = 0
-    print(f'Answer to part 2" {ans2}')
+    
+    print(f'Answer to part 2: {ans2}')
