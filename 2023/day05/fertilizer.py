67a4a90249f83359a99e07e9546a5bc8549df4b8
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-12-05 18:31:58 +0100

    solution to part II of day 05, 2023

--- 2023/day05/fertilizer.py
+++ 2023/day05/fertilizer.py
@@ -1,4 +1,6 @@
-""" Advent of Code 2023 -- Day 05 -- """
+""" Advent of Code 2023 -- Day 05 --
+Run time: about 10 s with my input data
+"""
 
 import sys
 sys.path.append('../../aux')
@@ -25,11 +27,36 @@ def parse_data(lines):
 
     return seeds, tables
 
-def new_map(x, table):
-    for cond in table:
-        if x >= cond[1] and x < cond[1]+cond[2]:
-            return cond[0] + x - cond[1]
-    return x
+def seed2loc(s, tables):
+    l = s
+    
+    for tab in tables:
+        for cond in tab:
+            if l >= cond[1] and l < cond[1]+cond[2]:
+                l = cond[0] + l - cond[1]
+                break
+
+    return l
+
+def loc2seed(l, tables):
+    # inverse of seed2loc 
+    m = len(tables)
+
+    s = l
+    for j in range(m):
+        for cond in tables[m-j-1]:
+            if s >= cond[0] and s < cond[0]+cond[2]:
+                s = cond[1] + s - cond[0]
+                break
+    return s
+
+def admissible(s, seeds):
+    m = len(seeds)//2
+
+    for j in range(m):
+        if s >= seeds[2*j] and s < seeds[2*j] + seeds[2*j+1]:
+            return True
+    return False
 
 if __name__ == '__main__':
     data = aoc.get_input('input1.txt')                                  
@@ -38,21 +65,22 @@ if __name__ == '__main__':
 
     # Part I    
     seeds, tables = parse_data(lines)
-    print(tables)
     locations = []
 
     for seed in seeds:
-        x = seed
-        for tab in tables:
-            x = new_map(x, tab)
-        locations.append(x)
+        locations.append(seed2loc(seed, tables))
 
     ans1 = min(locations)
 
     print(f'Answer to part 1: {ans1}')
 
     # Part II
-    
-    ans2 = 0
+    print("Computing solutions to part II...(takes a few seconds)")
+    l = 0
+    s = loc2seed(l, tables)
+    while not admissible(s, seeds):
+        l += 1
+        s = loc2seed(l, tables)
+    ans2 = l
     
     print(f'Answer to part 2: {ans2}')
