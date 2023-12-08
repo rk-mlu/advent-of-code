082bb17b5b8f1a087a4f0febf25b38cbe7a56691
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-12-08 14:55:53 +0100

    minor changes to the template

--- aux/template.py
+++ aux/template.py
@@ -5,18 +5,25 @@ sys.path.append('../../aux')
 import aoc
 # import numpy as np
 
-if __name__ == '__main__':
-    
-    # Part I    
-    data = aoc.get_input('input1.txt')                                  
+def parsing(data):
+    # write parser of input data    
     lines = data.splitlines()
+
+    return lines
+
+if __name__ == '__main__':
+    data = aoc.get_input('input1.txt')                                  
+    # data = aoc.get_input('input2.txt')
+    
+    lines = parsing(data)
+
+    # Part I    
 
     ans1 = 0
 
     print(f'Answer to part 1: {ans1}')
 
     # Part II
-    # data = aoc.get_input('input2.txt')
     
     ans2 = 0
     
