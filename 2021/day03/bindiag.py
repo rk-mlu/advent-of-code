9e75e6130f06cebb9c18a37bb501e2a4b5a7a48c
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-12-08 15:57:40 +0100

    aoc2021: completed day 03

--- 2021/day03/bindiag.py
+++ 2021/day03/bindiag.py
@@ -5,10 +5,41 @@ sys.path.append('../../aux')
 import aoc
 import numpy as np
 
+def filter_numbers(crit, lines):
+    m = len(lines)
+    n = len(lines[0])
+
+    B = np.zeros((m,n), dtype=int)
+
+    for i in range(m):
+        for j in range(n):
+            if lines[i][j] == '1':
+                B[i,j] = 1
+    valid = np.ones(m, dtype=int)
+    
+    col = 0
+    while np.sum(valid) > 1:
+        num = np.sum(valid)
+        
+        valid_rows = (valid == 1)
+
+        if (-1)**crit * 2*np.sum(B[valid_rows, col]) - crit < (-1)**crit*num:
+            # keep only 'crit'
+            valid[valid_rows] = valid[valid_rows]*B[valid_rows, col]
+        else :
+            valid[valid_rows] = valid[valid_rows]*(1 - B[valid_rows, col])
+        
+    
+        col += 1
+
+    return B[valid==1, :][0]
+    
+
 if __name__ == '__main__':
     
     # Part I    
     data = aoc.get_input('input1.txt')                                  
+    # data = aoc.get_input('input2.txt')                                  
     lines = data.splitlines()
     
     n = len(lines[0])
@@ -21,7 +52,6 @@ if __name__ == '__main__':
     gamma = 0
     epsilon = 0
     for j, val in enumerate(counters):
-        print(val, )
         if val > len(lines)//2:
             gamma += 2**(n-j-1)
         else:
@@ -33,5 +63,14 @@ if __name__ == '__main__':
     # Part II
     # data = aoc.get_input('input2.txt')
     
-    ans2 = 0
+    O2 = filter_numbers(1, lines)
+    CO2 = filter_numbers(0, lines)
+   
+    o2rat = 0
+    co2rat = 0
+    for j, (bO2, bCO2) in enumerate(zip(O2, CO2)):
+        o2rat += bO2*2**(n-j-1)
+        co2rat += bCO2*2**(n-j-1)
+    
+    ans2 = o2rat* co2rat
     print(f'Answer to part 2: {ans2}')
