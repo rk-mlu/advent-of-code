3f52e1809ef524c08bb3bbef3b1d3235e3a5cf82
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-12-09 18:09:35 +0100

    extend aux file: use aocd to download input

--- aux/aoc.py
+++ aux/aoc.py
@@ -1,7 +1,23 @@
-""" Advent of Code 2022  -- Helper functions """
+""" Advent of Code 2021-2023  -- Helper functions """
+import os.path
 
-def get_input(fname='input.txt'):
+def dl_data(day, year, fname="input1.txt"):
+
+    if not os.path.isfile(fname):
+        from aocd import get_data
+        
+        data = get_data(day=day, year=year)
+        
+        with open(fname, "w") as file:
+            file.write(data)
+    else :
+        data = get_input(fname)
+
+    return data
+    
+def get_input(fname='input1.txt'):
     """ read input file and return it as string """
+
     with open(fname, 'r') as file:
         data = file.read()
 
