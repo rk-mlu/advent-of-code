e518b717e001dbc410edb982c284ef7afd4f7e39
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-12-21 00:25:09 +0100

    Day 20 is WIP

---
+++ 2023/day20/pulse_propagation.py
@@ -0,0 +1,186 @@
+""" Advent of Code 2023 -- Day 20 -- """
+year = 2023
+day = 20     # set day!
+
+import sys
+sys.path.append('../../aux')
+import aoc
+# import numpy as np
+
+def parsing(data):
+    # parser for the input data    
+    lines = data.splitlines()
+    
+    modules = []
+
+    for line in lines:
+        i, o = line.split(' -> ')
+        if i[0] in ['%', '&']:
+            t = i[0]
+            lab = i[1:]
+        else :
+            t = None
+            lab = i
+        dests = o.split(', ')
+
+        modules.append((lab, t, dests))
+
+    return modules
+
+class ComModule:
+    def __init__(self, mod):
+        self.lab = mod[0]
+        self.type = mod[1]
+        self.dests = mod[2]
+        self.preds = set()
+        
+        if self.type == '%':
+            self.state = 0
+        elif self.type == '&':
+            self.state = dict()
+
+    def __str__(self):
+        s = f'Node {self.lab} is a {self.type} module relaying to {self.dests}.\n'
+        s+= f'Preds are {self.preds}'
+        return s
+    
+    def process_pulse(self, orig, hl):
+        new_pulses = []
+        
+        if self.type is None:
+            for d in self.dests:
+                new_pulses.append((self.lab, d, hl))
+                # print(f'{self.lab} {hl} -> {d}')
+                
+        if self.type == '%':
+            if hl == 0:
+                self.state = 1 - self.state
+                hl_new = self.state
+                for d in self.dests:
+                    new_pulses.append((self.lab, d, hl_new))
+                    # print(f'{self.lab} {hl_new} -> {d}')
+
+        if self.type == '&':
+            self.state[orig] = hl
+
+            all_high = 1
+            for p in self.preds:
+                all_high *= self.state[p]
+            if all_high:
+                hl_new = 0
+            else :
+                hl_new = 1
+
+            for d in self.dests:
+                new_pulses.append((self.lab, d, hl_new))
+                # print(f'{self.lab} {hl_new} -> {d}')
+
+        return new_pulses
+
+def det_preds(network):
+    new_nodes = []
+    for node in network:
+        # print(node)
+        for d in network[node].dests:
+            if d in network.keys():
+                network[d].preds.add(node)
+            else :
+                new_nodes.append(d)
+    for d in new_nodes:
+        network[d] = ComModule((d, None, []))
+
+    if len(new_nodes) > 0:
+        det_preds(network)
+
+    for node in network:
+        if network[node].type == '&':
+            for p in network[node].preds:
+                network[node].state[p] = 0
+
+def part2(network):
+    dest_dh = []
+    dest_mk = []
+    dest_vf = []
+    dest_rn = []
+    j = 0
+    while True:
+        Q = [('button', 'broadcaster', 0)]
+        j += 1
+        dest_rx = []
+
+        while len(Q) > 0:
+            pulse = Q.pop(0)
+
+            new_pulses = network[pulse[1]].process_pulse(pulse[0],pulse[2])
+            for p in new_pulses:
+                if p[1] == 'rx':
+                    dest_rx.append(p)
+                if p[1] == 'dh' and p[2]==1:
+                    dest_dh.append(j)
+                if p[1] == 'mk' and p[2]==1:
+                    dest_mk.append(j)
+                if p[1] == 'vf' and p[2]==1:
+                    dest_vf.append(j)
+                if p[1] == 'rn' and p[2]==1:
+                    dest_rn.append(j)
+
+            Q = Q + new_pulses
+            
+        if len(dest_rx) > 0:
+            for p in dest_rx:
+                if p[2] == 0:
+                    print(j, dest_rx)
+            if len(dest_rx) == 1 and dest_rx[0][2] == 0:
+                break
+        if len(dest_rn) > 100:
+            print(dest_dh, dest_mk, dest_vf, dest_rn) 
+            break
+    return j
+
+
+if __name__ == '__main__':
+    data = aoc.dl_data(day, year, 'input1.txt')                                  
+    # data = aoc.get_input('input3.txt')
+    
+    modules = parsing(data)
+
+    # Part I    
+
+    network = dict()
+    for m in modules:
+        network[m[0]] = ComModule(m)
+
+    det_preds(network)
+
+    
+    num_l = 0
+    num_h = 0
+
+    for j in range(1000):
+        Q = [('button', 'broadcaster', 0)]
+
+        while len(Q) > 0:
+            pulse = Q.pop(0)
+
+            if pulse[2] == 0:
+                num_l += 1
+            else :
+                num_h += 1
+            
+            Q = Q + network[pulse[1]].process_pulse(pulse[0],pulse[2])
+    
+    ans1 = num_l*num_h
+
+    print(f'Answer to part 1: {ans1}')
+
+    # Part II
+    modules = parsing(data)
+    network = dict()
+    for m in modules:
+        network[m[0]] = ComModule(m)
+
+    det_preds(network)
+    
+    ans2 = part2(network)
+
+    print(f'Answer to part 2: {ans2}')
