57f897614cc500eb0dc8e91f2da65cc3e1133880
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2023-12-22 22:57:04 +0100

    Solution to day 21, part II

--- 2023/day21/step_counter.py
+++ 2023/day21/step_counter.py
@@ -22,19 +22,17 @@ def parsing(data):
                 garden[i+1,j+1] = 1
             if line[j] == 'S':
                 pos = (i+1,j+1)
+    # print(np.count_nonzero(garden))
     
     return pos, garden
 
-def get_nb(pos_set, garden, look_up_table=dict()):
+def get_nb(pos_set, garden):
     M, N = garden.shape
     
     nbs = set()
 
     while len(pos_set) > 0:
         p = pos_set.pop()
-
-        if p in look_up_table.keys():
-            nbs = look_up_table[p]
         
         for d in [(0,1), (0,-1), (1,0), (-1,0)]:
             cand = (p[0] + d[0], p[1] + d[1])
@@ -43,33 +41,92 @@ def get_nb(pos_set, garden, look_up_table=dict()):
 
     return nbs
 
+def get_nb2(pos_set, garden):
+    M, N = garden.shape
+    
+    nbs = set()
+
+    while len(pos_set) > 0:
+        p = pos_set.pop()
+        
+        for d in [(0,1), (0,-1), (1,0), (-1,0)]:
+            
+            c0 = p[0] + d[0]
+            c1 = p[1] + d[1]
+            c2 = p[2]
+            c3 = p[3]
+
+            if c0 == -1:
+                c0 = M-1
+                c2 -= 1
+            if c0 == M:
+                c0 = 0
+                c2 += 1
+            if c1 == -1:
+                c1 = N-1
+                c3 -= 1
+            if c1 == N:
+                c1 = 0
+                c3 += 1
+
+            cand = (c0, c1, c2, c3)
+
+            if garden[(cand[0],cand[1])] == 1:
+                nbs.add(cand)
+
+    return nbs
+
+def part1(start_pos, steps):
+    reachable = {start_pos}
+
+    for j in range(steps):
+        reachable = get_nb(reachable, garden) 
+
+    return len(reachable)
+
+
+
 if __name__ == '__main__':
     data = aoc.dl_data(day, year, 'input1.txt')                                  
     # data = aoc.get_input('input2.txt')
     
-    pos, garden = parsing(data)
+    pos1, garden = parsing(data)
 
     M, N = garden.shape
-    print(M,N, pos)
 
     # Part I    
     
-    reachable = {pos}
-    
-    look_up_tab = dict()
+    steps = 64
 
-
-    steps = 26501365
-    for j in range(steps):
-        reachable = get_nb(reachable, garden) 
-        # print(j+1,reachable)
-
-    ans1 = len(reachable)
+    ans1 = part1(pos1, steps)
 
     print(f'Answer to part 1: {ans1}')
+    
 
     # Part II
     
-    ans2 = 0
+    steps = 26501365 - 65
+    multiple = steps // 131 
+
+    odd = part1((66,66), 131)
+    even = part1((66,66),132)
+    
+    # center
+    ans2 = odd 
+    
+    # fully filled neighboring tiles (2 periodic odd and even)
+    ans2 += (multiple-2)*(multiple)*odd + (multiple)**2*even
+
+    # boundary corners
+    ans2 += part1((66,1), 130) + part1((1,66), 130) + part1((131,66), 130)
+    ans2 += part1((66,131), 130)
+    
+    # diagonal boundary
+    d1 = multiple * part1( (1,1), 64) + (multiple - 1)*part1( (1,1), 64+131)
+    d2 = multiple * part1( (131,131), 64) + (multiple - 1)* part1( (131,131), 64+131)
+    d3 = multiple * part1( (131,1), 64) + (multiple - 1)*part1( (131,1), 64+131)
+    d4 = multiple * part1( (1,131), 64) + (multiple - 1)*part1( (1,131), 64+131)
+    
+    ans2 += d1 + d2 + d3 + d4
     
     print(f'Answer to part 2: {ans2}')
