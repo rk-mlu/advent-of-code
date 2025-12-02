1c7b6eee6d7fa53539d36a2f6b07577677b9f0f2
Raphael Kruse <65403623+rk-mlu@users.noreply.github.com>
2025-12-02 21:24:17 +0100

    prepare for day 03

---
+++ 2025/day03/day03.py
@@ -0,0 +1,32 @@
+""" Advent of Code 2025 -- Day 3 -- """
+year = 2025
+day = 3         # set day!
+
+import sys
+sys.path.append('../../aux')
+import aoc
+# import numpy as np
+
+def parsing(data):
+    # parser for the input data    
+    lines = data.splitlines()
+
+    return lines
+
+if __name__ == '__main__':
+    data = aoc.dl_data(day, year, 'input1.txt')                                  
+    # data = aoc.get_input('input0.txt')
+    
+    lines = parsing(data)
+
+    # Part I    
+
+    ans1 = 0
+
+    print(f'Answer to part 1: {ans1}')
+
+    # Part II
+    
+    ans2 = 0
+    
+    print(f'Answer to part 2: {ans2}')
